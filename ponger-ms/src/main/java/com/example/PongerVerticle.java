98319d2c2bed280a81fc815f2fa6b54332f8da66
Giacomo <giacomo.venturini2@studio.unibo.it>
2019-07-20 00:44:01 +0200

    add cors handling

--- ponger-ms/src/main/java/com/example/PongerVerticle.java
+++ ponger-ms/src/main/java/com/example/PongerVerticle.java
@@ -1,6 +1,10 @@
 package com.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.DecodeException;
@@ -8,84 +12,107 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CorsHandler;
 
 public class PongerVerticle extends AbstractVerticle {
 
-	private int pongSentCount = 0;
+    private int pongSentCount = 0;
 
-	@Override
-	public void start() {
-		Router router = Router.router(vertx);
+    @Override
+    public void start() {
+        Router router = Router.router(vertx);
 
-		router.route().handler(BodyHandler.create());
+        router.route().handler(BodyHandler.create());
 
-		// REST API
-		router.get("/api/status").handler(this::getStatus);
-		router.post("/api/ping").handler(this::pingHandler);
+        // CORS handling
+        Set<String> allowedHeaders = new HashSet<>();
+        allowedHeaders.add("x-requested-with");
+        allowedHeaders.add("Access-Control-Allow-Origin");
+        allowedHeaders.add("origin");
+        allowedHeaders.add("Content-Type");
+        allowedHeaders.add("accept");
+        allowedHeaders.add("X-PINGARUNER");
 
-		HttpServer server = vertx.createHttpServer();
-		server.requestHandler(router);
+        Set<HttpMethod> allowedMethods = new HashSet<>();
+        allowedMethods.add(HttpMethod.GET);
+        allowedMethods.add(HttpMethod.POST);
+        allowedMethods.add(HttpMethod.OPTIONS);
+        allowedMethods.add(HttpMethod.DELETE);
+        allowedMethods.add(HttpMethod.PATCH);
+        allowedMethods.add(HttpMethod.PUT);
 
-		int verticlePort = 8080;
-		server.listen(verticlePort, listenHandler -> {
-			if (listenHandler.succeeded()) {
-				System.out.println(PongerVerticle.class.getSimpleName() + " running on port " + verticlePort);
-			} else {
-				System.out.println("Error in service setup");
-				System.out.println("" + listenHandler.cause());
-				vertx.close();
-			}
-		});
-	}
+        CorsHandler corsHandler = CorsHandler.create("*");
+        corsHandler.allowedHeaders(allowedHeaders);
+        corsHandler.allowedMethods(allowedMethods);
+        router.route().handler(corsHandler);
 
-	private void getStatus(RoutingContext routingContext) {
-		HttpServerResponse response = routingContext.response();
-		response.putHeader("Content-Type", "application/text");
-		response.end(PongerVerticle.class.getSimpleName() + " status: active");
-	}
+        // REST API
+        router.get("/api/status").handler(this::getStatus);
+        router.post("/api/ping").handler(this::pingHandler);
 
-	private void pingHandler(RoutingContext routingContext) {
-		HttpServerResponse response = routingContext.response();
-		JsonObject jsonBody;
-		try {
-			String body = routingContext.getBodyAsString();
-			jsonBody = new JsonObject(body);
-		} catch (NullPointerException | DecodeException e) {
-			sendWrongBodyDataReceivedMsg(response);
-			return;
-		}
+        HttpServer server = vertx.createHttpServer();
+        server.requestHandler(router);
 
-		if (jsonBody.containsKey("ping")) {
-			System.out.println("received ping " + jsonBody.getInteger("ping"));
-			response.putHeader("Content-Type", "application/json");
+        int verticlePort = 8080;
+        server.listen(verticlePort, listenHandler -> {
+            if (listenHandler.succeeded()) {
+                System.out.println(PongerVerticle.class.getSimpleName() + " running on port " + verticlePort);
+            } else {
+                System.out.println("Error in service setup");
+                System.out.println("" + listenHandler.cause());
+                vertx.close();
+            }
+        });
+    }
 
-			vertx.executeBlocking(promise -> {
-				sleep(2000);
-				promise.complete();
-			}, result -> {
-				JsonObject jsonResponse = new JsonObject();
-				jsonResponse.put("pong", ++pongSentCount);
-				response.end(jsonResponse.encode());
-				System.out.println("sent pong " + pongSentCount);
-			});
-		} else {
-			sendWrongBodyDataReceivedMsg(response);
-		}
-	}
+    private void getStatus(RoutingContext routingContext) {
+        HttpServerResponse response = routingContext.response();
+        response.putHeader("Content-Type", "application/text");
+        response.end(PongerVerticle.class.getSimpleName() + " status: active");
+    }
 
-	private void sleep(long millis) {
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    private void pingHandler(RoutingContext routingContext) {
+        HttpServerResponse response = routingContext.response();
+        JsonObject jsonBody;
+        try {
+            String body = routingContext.getBodyAsString();
+            jsonBody = new JsonObject(body);
+        } catch (NullPointerException | DecodeException e) {
+            sendWrongBodyDataReceivedMsg(response);
+            return;
+        }
 
-	private void sendWrongBodyDataReceivedMsg(HttpServerResponse response) {
-		String reason = "wrong body data received";
-		System.out.println(reason);
-		response.setStatusCode(400);
-		response.putHeader("Content-Type", "application/text");
-		response.end(reason);
-	}
+        if (jsonBody.containsKey("ping")) {
+            System.out.println("received ping " + jsonBody.getInteger("ping"));
+            response.putHeader("Content-Type", "application/json");
+
+            vertx.executeBlocking(promise -> {
+                sleep(2000);
+                promise.complete();
+            }, result -> {
+                JsonObject jsonResponse = new JsonObject();
+                jsonResponse.put("pong", ++pongSentCount);
+                response.end(jsonResponse.encode());
+                System.out.println("sent pong " + pongSentCount);
+            });
+        } else {
+            sendWrongBodyDataReceivedMsg(response);
+        }
+    }
+
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendWrongBodyDataReceivedMsg(HttpServerResponse response) {
+        String reason = "wrong body data received";
+        System.out.println(reason);
+        response.setStatusCode(400);
+        response.putHeader("Content-Type", "application/text");
+        response.end(reason);
+    }
 }
