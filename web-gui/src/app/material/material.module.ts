e8a4874a8c732f85aa02e106a308226f9d99784d
Giacomo <giacomo.venturini2@studio.unibo.it>
2019-07-19 18:57:30 +0200

    add Angular material support

---
+++ web-gui/src/app/material/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
+
+const materialModules = [
+  BrowserAnimationsModule,
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    materialModules
+  ],
+  exports: [
+    materialModules
+  ]
+})
+export class MaterialModule {
+}
