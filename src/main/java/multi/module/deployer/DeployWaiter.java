01b7a033a7493a201d8de6d02ae16a9ff8d1394e
Giacomo <giacomo.venturini2@studio.unibo.it>
2019-07-27 11:13:11 +0200

    add class to wait for microservices setup

---
+++ src/main/java/multi/module/deployer/DeployWaiter.java
@@ -0,0 +1,85 @@
+package multi.module.deployer;
+
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.mqtt.MqttClient;
+
+
+public class DeployWaiter {
+
+    private final Vertx vertx;
+    private long retryDelay = 2000;
+
+    public DeployWaiter() {
+        this(Vertx.vertx());
+    }
+
+    public DeployWaiter(Vertx vertx) {
+        this.vertx = vertx;
+    }
+
+    public Future<Void> waitHttpDeployment(int port, String host, String requestURI) {
+        System.out.println("Waiting setup via http");
+        Promise<Void> promise = Promise.promise();
+        WebClient client = WebClient.create(vertx);
+        waitHttpDeployment(promise, client, port, host, requestURI);
+        return promise.future();
+    }
+
+    private void waitHttpDeployment(Promise<Void> promise, WebClient client, int port, String host, String requestURI) {
+        client.get(port, host, requestURI).send(ar -> {
+            if (ar.succeeded()) {
+                promise.complete();
+                client.close();
+            } else {
+                vertx.setTimer(retryDelay, r -> waitHttpDeployment(promise, client, port, host, requestURI));
+            }
+        });
+    }
+
+    public Future<Void> waitWebsocketDeployment(int port, String host, String requestURI) {
+        System.out.println("Waiting setup via websocket");
+        Promise<Void> promise = Promise.promise();
+        HttpClient client = vertx.createHttpClient();
+        waitWebsocketDeployment(promise, client, port, host, requestURI);
+        return promise.future();
+    }
+
+    private void waitWebsocketDeployment(Promise<Void> promise, HttpClient client, int port, String host, String requestURI) {
+        client.webSocket(port, host, requestURI, res -> {
+            if (res.succeeded()) {
+                promise.complete();
+                client.close();
+            } else {
+                vertx.setTimer(retryDelay, r -> waitWebsocketDeployment(promise, client, port, host, requestURI));
+            }
+        });
+    }
+
+    public Future<Void> waitMqttDeployment(int port, String host) {
+        Promise<Void> promise = Promise.promise();
+        MqttClient client = MqttClient.create(vertx);
+        waitMqttDeployment(promise, client, port, host);
+        return promise.future();
+    }
+
+    private void waitMqttDeployment(Promise<Void> promise, MqttClient client, int port, String host) {
+        System.out.println("Waiting setup via mqtt");
+        client.connect(port, host, s -> {
+            if (s.succeeded()) {
+                promise.complete();
+                client.disconnect();
+            } else {
+                System.out.println("Something went wrong ");
+                vertx.setTimer(retryDelay, r -> waitMqttDeployment(promise, client, port, host));
+            }
+        });
+    }
+
+    public void deployCompleted() {
+        vertx.close();
+    }
+}
