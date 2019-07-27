2c446f103c018c4797f07a022c97f56207a0069d
Giacomo <giacomo.venturini2@studio.unibo.it>
2019-07-27 19:49:48 +0200

    add library entrypoint

---
+++ src/main/java/multi/module/deployer/MultiModuleDeployer.java
@@ -0,0 +1,49 @@
+package multi.module.deployer;
+
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
+import multi.module.deployer.cmdrunner.CmdRunner;
+import multi.module.deployer.cmdrunner.CmdRunnerImpl;
+import multi.module.deployer.moduleconfig.ModuleConfig;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiModuleDeployer {
+
+    private final List<ModuleConfig[]> moduleConfigList = new ArrayList<>();
+    private final CmdRunner cmdRunner = new CmdRunnerImpl();
+    private final DeployWaiter deployWaiter = new DeployWaiter();
+
+    public CmdRunner getCmdRunner() {
+        return cmdRunner;
+    }
+
+    public DeployWaiter getDeployWaiter() {
+        return deployWaiter;
+    }
+
+    public MultiModuleDeployer add(ModuleConfig... a) {
+        moduleConfigList.add(a);
+        return this;
+    }
+
+    public void deploy() {
+        deploy(0);
+    }
+
+    private void deploy(int idx) {
+        if (idx < moduleConfigList.size()) {
+            List<Future> futures = new ArrayList<>();
+            ModuleConfig[] cmdSetups = this.moduleConfigList.get(idx);
+            for (ModuleConfig cmdSetup : cmdSetups) {
+                cmdRunner.execInNewTerm(cmdSetup.getUnixCmd(), cmdSetup.getWindowsCmd());
+                Future deploymentFuture = cmdSetup.waitDeployment(deployWaiter);
+                futures.add(deploymentFuture);
+            }
+            CompositeFuture.all(futures).setHandler(h -> deploy(idx + 1));
+        } else {
+            deployWaiter.deployCompleted();
+        }
+    }
+}
