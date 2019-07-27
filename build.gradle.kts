6dd37618ca01653720fb8ef5a96ad0ead0a5fa58
Giacomo <giacomo.venturini2@studio.unibo.it>
2019-07-27 11:12:05 +0200

    add vertx support

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,18 @@ plugins {
 }
 
 repositories {
+    mavenCentral()
     jcenter()
 }
 
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.9")
+    val vertxVersion = "3.8.0"
+    implementation("io.vertx:vertx-core:$vertxVersion")
+    implementation("io.vertx:vertx-web-client:$vertxVersion")
+    implementation("io.vertx:vertx-mqtt:$vertxVersion")
 
     val junitVersion = "5.3.1"
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
