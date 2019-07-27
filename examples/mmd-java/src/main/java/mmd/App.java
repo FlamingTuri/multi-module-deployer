e714c41738562630dfc55a76b75c5db399316cc6
Giacomo <giacomo.venturini2@studio.unibo.it>
2019-07-27 11:23:28 +0200

    update example with the new way to wait for micorservice initialization

--- examples/mmd-java/src/main/java/mmd/App.java
+++ examples/mmd-java/src/main/java/mmd/App.java
@@ -1,9 +1,15 @@
 package mmd;
 
+import io.vertx.core.Future;
 import multi.module.deployer.CmdRunner;
 import multi.module.deployer.CmdRunnerImpl;
+import multi.module.deployer.DeployWaiter;
 
 public class App {
+
+    private static String commonCmd;
+    private static String linuxCmd;
+    private static String windowsCmd;
 
     private static void sleep(long millis) {
         try {
@@ -14,46 +20,48 @@ public class App {
     }
 
     public static void main(String[] args) {
-        String commonCmd;
-        String linuxCmd;
-        String windowsCmd;
-        CmdRunner entryPoint = new CmdRunnerImpl();
+        CmdRunner cmdRunner = new CmdRunnerImpl();
+        DeployWaiter dw = new DeployWaiter();
 
-        // download dependencies and build ponger-ms
-        linuxCmd = "cd ../ponger-ms; ./gradlew build";
-        windowsCmd = "cd ..\\ponger-ms & gradlew.bat build";
-        entryPoint.exec(linuxCmd, windowsCmd);
+        Future.future(v -> {
+            // download dependencies and build ponger-ms
+            linuxCmd = "cd ../ponger-ms; ./gradlew build";
+            windowsCmd = "cd ..\\ponger-ms & gradlew.bat build";
+            cmdRunner.exec(linuxCmd, windowsCmd);
 
-        // download pinger-ms dependencies
-        commonCmd = "npm install";
-        linuxCmd = "cd ../pinger-ms; " + commonCmd;
-        windowsCmd = "cd ..\\pinger-ms & " + commonCmd;
-        entryPoint.exec(linuxCmd, windowsCmd);
+            // download pinger-ms dependencies
+            commonCmd = "npm install";
+            linuxCmd = "cd ../pinger-ms; " + commonCmd;
+            windowsCmd = "cd ..\\pinger-ms & " + commonCmd;
+            cmdRunner.exec(linuxCmd, windowsCmd);
 
-        // download web-gui dependencies
-        linuxCmd = "cd ../web-gui; " + commonCmd;
-        windowsCmd = "cd ..\\web-gui & " + commonCmd;
-        entryPoint.exec(linuxCmd, windowsCmd);
-
-        // run ponger-ms
-        linuxCmd = "cd ../ponger-ms; ./gradlew run";
-        windowsCmd = "cd ..\\ponger-ms & gradlew.bat run";
-        entryPoint.execInNewTerm(linuxCmd, windowsCmd);
-        // wait for it's setup
-        sleep(5000);
-
-        // run pinger-ms
-        commonCmd = "node app.js";
-        linuxCmd = "cd ../pinger-ms; " + commonCmd;
-        windowsCmd = "cd ..\\pinger-ms & " + commonCmd;
-        entryPoint.execInNewTerm(linuxCmd, windowsCmd);
-        // wait for it's setup
-        sleep(3000);
-
-        // run web-gui
-        commonCmd = "npx ng serve -o --poll=3000";
-        linuxCmd = "cd ../web-gui; " + commonCmd;
-        windowsCmd = "cd ..\\web-gui & " + commonCmd;
-        entryPoint.execInNewTerm(linuxCmd, windowsCmd);
+            // download web-gui dependencies
+            linuxCmd = "cd ../web-gui; " + commonCmd;
+            windowsCmd = "cd ..\\web-gui & " + commonCmd;
+            cmdRunner.exec(linuxCmd, windowsCmd);
+        }).compose(v -> {
+            // run ponger-ms
+            linuxCmd = "cd ../ponger-ms; ./gradlew run";
+            windowsCmd = "cd ..\\ponger-ms & gradlew.bat run";
+            cmdRunner.execInNewTerm(linuxCmd, windowsCmd);
+            // wait for ponger-ms setup
+            return dw.waitHttpDeployment(8080, "localhost", "/api/status");
+        }).compose(v -> {
+            // run pinger-ms
+            commonCmd = "node app.js";
+            linuxCmd = "cd ../pinger-ms; " + commonCmd;
+            windowsCmd = "cd ..\\pinger-ms & " + commonCmd;
+            cmdRunner.execInNewTerm(linuxCmd, windowsCmd);
+            // wait for pinger-ms setup
+            return dw.waitHttpDeployment(3000, "localhost", "/api/status");
+        }).setHandler(a -> {
+            // run web-gui
+            commonCmd = "npx ng serve -o --poll=3000";
+            linuxCmd = "cd ../web-gui; " + commonCmd;
+            windowsCmd = "cd ..\\web-gui & " + commonCmd;
+            cmdRunner.execInNewTerm(linuxCmd, windowsCmd);
+            // close vertx instance and lets the app terminate
+            dw.deployCompleted();
+        });
     }
 }
