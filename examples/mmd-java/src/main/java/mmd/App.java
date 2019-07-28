0eaf16545c1d04ff97dfe22853bb3bf27329f754
Giacomo <giacomo.venturini2@studio.unibo.it>
2019-07-28 13:59:09 +0200

    update example

--- examples/mmd-java/src/main/java/mmd/App.java
+++ examples/mmd-java/src/main/java/mmd/App.java
@@ -1,9 +1,8 @@
 package mmd;
 
-import io.vertx.core.Future;
-import multi.module.deployer.CmdRunner;
-import multi.module.deployer.CmdRunnerImpl;
-import multi.module.deployer.DeployWaiter;
+import multi.module.deployer.MultiModuleDeployer;
+import multi.module.deployer.moduleconfig.ModuleConfig;
+import multi.module.deployer.moduleconfig.ModuleConfigFactory;
 
 public class App {
 
@@ -11,57 +10,46 @@ public class App {
     private static String linuxCmd;
     private static String windowsCmd;
 
-    private static void sleep(long millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-        CmdRunner cmdRunner = new CmdRunnerImpl();
-        DeployWaiter dw = new DeployWaiter();
+        MultiModuleDeployer multiModuleDeployer = new MultiModuleDeployer();
 
-        Future.future(v -> {
-            // download dependencies and build ponger-ms
-            linuxCmd = "cd ../ponger-ms; ./gradlew build";
-            windowsCmd = "cd ..\\ponger-ms & gradlew.bat build";
-            cmdRunner.exec(linuxCmd, windowsCmd);
+        // ponger-ms dependencies installation commands
+        linuxCmd = "cd ../ponger-ms; pwd; ./gradlew build";
+        windowsCmd = "cd ..\\ponger-ms & gradlew.bat build";
+        ModuleConfig pongerBuild = ModuleConfigFactory.setupModuleConfig(linuxCmd, windowsCmd);
 
-            // download pinger-ms dependencies
-            commonCmd = "npm install";
-            linuxCmd = "cd ../pinger-ms; " + commonCmd;
-            windowsCmd = "cd ..\\pinger-ms & " + commonCmd;
-            cmdRunner.exec(linuxCmd, windowsCmd);
+        // pinger-ms dependencies installation commands
+        commonCmd = "npm install";
+        linuxCmd = "cd ../pinger-ms; " + commonCmd;
+        windowsCmd = "cd ..\\pinger-ms & " + commonCmd;
+        ModuleConfig pingerBuild = ModuleConfigFactory.setupModuleConfig(linuxCmd, windowsCmd);
 
-            // download web-gui dependencies
-            linuxCmd = "cd ../web-gui; " + commonCmd;
-            windowsCmd = "cd ..\\web-gui & " + commonCmd;
-            cmdRunner.exec(linuxCmd, windowsCmd);
-        }).compose(v -> {
-            // run ponger-ms
-            linuxCmd = "cd ../ponger-ms; ./gradlew run";
-            windowsCmd = "cd ..\\ponger-ms & gradlew.bat run";
-            cmdRunner.execInNewTerm(linuxCmd, windowsCmd);
-            // wait for ponger-ms setup
-            return dw.waitHttpDeployment(8080, "localhost", "/api/status");
-        }).compose(v -> {
-            // run pinger-ms
-            commonCmd = "node app.js";
-            linuxCmd = "cd ../pinger-ms; " + commonCmd;
-            windowsCmd = "cd ..\\pinger-ms & " + commonCmd;
-            cmdRunner.execInNewTerm(linuxCmd, windowsCmd);
-            // wait for pinger-ms setup
-            return dw.waitHttpDeployment(3000, "localhost", "/api/status");
-        }).setHandler(a -> {
-            // run web-gui
-            commonCmd = "npx ng serve -o --poll=3000";
-            linuxCmd = "cd ../web-gui; " + commonCmd;
-            windowsCmd = "cd ..\\web-gui & " + commonCmd;
-            cmdRunner.execInNewTerm(linuxCmd, windowsCmd);
-            // close vertx instance and lets the app terminate
-            dw.deployCompleted();
-        });
+        // web-gui dependencies installation commands
+        linuxCmd = "cd ../web-gui; pwd; " + commonCmd;
+        windowsCmd = "cd ..\\web-gui & " + commonCmd;
+        ModuleConfig webGuiBuild = ModuleConfigFactory.setupModuleConfig(linuxCmd, windowsCmd);
+
+        multiModuleDeployer.add(pongerBuild, pingerBuild, webGuiBuild);
+
+        // commands to run ponger-ms
+        linuxCmd = "cd ../ponger-ms; ./gradlew run";
+        windowsCmd = "cd ..\\ponger-ms & gradlew.bat run";
+        multiModuleDeployer.add(ModuleConfigFactory.httpModuleConfig(linuxCmd, windowsCmd, 8080, "localhost", "/api/status"));
+
+        // commands to run pinger-ms
+        commonCmd = "node app.js";
+        linuxCmd = "cd ../pinger-ms; " + commonCmd;
+        windowsCmd = "cd ..\\pinger-ms & " + commonCmd;
+        ModuleConfig pingerConfig = ModuleConfigFactory.httpModuleConfig(linuxCmd, windowsCmd, 3000, "localhost", "/api/status");
+
+        // commands to run web-gui
+        commonCmd = "npx ng serve -o --poll=3000";
+        linuxCmd = "cd ../web-gui; " + commonCmd;
+        windowsCmd = "cd ..\\web-gui & " + commonCmd;
+        ModuleConfig webGuiConfig = ModuleConfigFactory.noDeploymentWaitModuleConfig(linuxCmd, windowsCmd);
+
+        multiModuleDeployer.add(pingerConfig, webGuiConfig);
+
+        multiModuleDeployer.deploy();
     }
 }
